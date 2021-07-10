fbc545e14409201d509b9f3ec4b0306921e2b917
Mario Rodriguez <mariordgez@hotmail.com>
2021-07-09 20:16:56 -0500

    Add attendee model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_155234) do
+ActiveRecord::Schema.define(version: 2021_07_09_214637) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.string "userame"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attendees_on_event_id"
+    t.index ["user_id"], name: "index_attendees_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -34,5 +44,7 @@ ActiveRecord::Schema.define(version: 2021_07_09_155234) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendees", "events"
+  add_foreign_key "attendees", "users"
   add_foreign_key "events", "users"
 end

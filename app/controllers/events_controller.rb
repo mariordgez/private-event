83df6578ac4d47c0740fe8e0061b74ed36ceaadc
Mario Rodriguez <mariordgez@hotmail.com>
2021-07-13 10:33:11 -0500

    Fix event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,6 +68,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :date, :user_id)
+      params.require(:event).permit(:title, :description, :date)
     end
 end

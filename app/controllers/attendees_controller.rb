6837664466bb676e7725e762b8438fdf9a7f15e7
Mario Rodriguez <mariordgez@hotmail.com>
2021-07-13 13:37:55 -0500

    Add attendee functionality to events

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,24 +2,18 @@ class AttendeesController < ApplicationController
   def new
     helpers.signed_in_only
     #@event = Event.new
-    @attendee= Attendee.new
+    @attendee= @event.attendees.build
   end
 
   def create
     helpers.signed_in_only
-    @attendee= Attendee.new(user_id=current_user.id, event_id=event_event_id, userame=current_user.email)
-   
+    @event = Event.find(params[:event_id])
+    @attendee= @event.attendees.build(attendee_params)
+    redirect_to event_path(@event)
 
-
-    respond_to do |format|
-      if @attendee.save
-        format.html { redirect_to @attendee, notice: "Event was successfully created." }
-        format.json { render :show, status: :created, location: @attendee }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @attendee.errors, status: :unprocessable_entity }
-      end
-    end
+  end
+  def attendee_params
+    params.require(:attendee).permit(:userame, :user_id)
   end
 
 end

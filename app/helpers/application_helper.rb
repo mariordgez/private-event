c4a461f77c80d21720ab4b13489734b0b2c0b725
Mario Rodriguez <mariordgez@hotmail.com>
2021-07-16 11:57:17 -0500

    Add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
    end
    def sign_in_out
     if user_signed_in?
-      link_to 'Sign Out', destroy_user_session_path, method: :delete
+      link_to 'Sign Out', destroy_user_session_path, method: :delete, class: "nav-link active"
     else
-      link_to 'Sign In', new_user_session_path
+      link_to 'Sign In', new_user_session_path, class: "nav-link active"
     end
   end
 end

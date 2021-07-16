c47a731c9d599489e4faf09a974d2239969fcf6b
Mario Rodriguez <mariordgez@hotmail.com>
2021-07-16 15:36:03 -0500

    Added unit test for model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendees, inverse_of: :attended_event, dependent: :destroy
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
   scope :past_event, -> { where('date < ?', Date.current) }
   scope :upcoming_event, -> { where('date >= ?', Date.current) }
 end
